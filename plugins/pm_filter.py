45b80159c92411e8c402fb9a1ccdba03a3c5bd4f
samurai-maker <84018991+samurai-maker@users.noreply.github.com>
2022-04-12 06:53:02 -0700

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -403,14 +403,12 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "pages":
         await query.answer()
     elif query.data == "start":
-        buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-        ], [
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/TeamEvamaria')
-        ], [
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+        buttons =  [[
+            
+            InlineKeyboardButton('Search 🌎', switch_inline_query_current_chat='')
+            ],[
+            InlineKeyboardButton('Help 🔧', callback_data='help'),
+            InlineKeyboardButton(' About 🎧', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
