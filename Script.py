9cc8eb15f62b89158818b8f31b91972ec8c39f27
samurai-maker <84018991+samurai-maker@users.noreply.github.com>
2022-04-13 02:15:33 -0700

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,20 +1,24 @@
 class script(object):
-    START_TXT = """ഹാലോ  {},
-എന്റെ പേര്  <a href=https://t.me/{}>{}</a>,ഞാൻ നിങ്ങൾക് മൂവി തരാം , എന്നെ ഒന്ന് ഗ്രൂപ്പിൽ ചേർത്തു നോക്കുന്നെ 😍"""
-    HELP_TXT = """ഹേയ്{}
-എന്റെ സഹായഹസ്തം ചുവടെ   ."""
+    START_TXT = """<b>ഹാലോ  {},
+എന്റെ പേര്  <a href=https://t.me/{}>{}</a>,ഞാൻ നിങ്ങൾക് മൂവി തരാം , എന്നെ ഒന്ന് ഗ്രൂപ്പിൽ ചേർത്തു നോക്കുന്നെ 🤩</b>"""
+    HELP_TXT = """<b>ഹേയ്{}
+എന്റെ സഹായഹസ്തം ചുവടെ </b>"""
     ABOUT_TXT = """
-<a href=https://t.me/slogan_98/393>
+▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬
+<a href=https://t.me/slogan_98/422>
 ┏┓
 ┃┃
 ┃┃┏━━┳┓┏┳━━┓┏┓┏┓
 ┃┃┃┏┓┃┗┛┃┃━┫┃┃┃┃
 ┃┗┫┗┛┣┓┏┫┃━┫┃┗┛┃
-┗━┻━━┛┗┛┗━━┛┗━━┛</a>
+┗━┻━━┛┗┛┗━━┛┗━━┛ 🌹 </a>
+▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬
 
 """
     SOURCE_TXT = """<b> PRIVATE </b>"""
-    MANUELFILTER_TXT = """Help: <b>Filters</b>
+    MANUELFILTER_TXT = """
+▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬    
+Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and bot will respond whenever a keyword is found the message
 
@@ -27,8 +31,11 @@ class script(object):
 • /filter - <code>add a filter in chat</code>
 • /filters - <code>list all the filters of a chat</code>
 • /del - <code>delete a specific filter in chat</code>
-• /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
-    BUTTON_TXT = """Help: <b>Buttons</b>
+• /delall - <code>delete the whole filters in a chat (chat owner only)</code>
+▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬"""
+    BUTTON_TXT = """
+ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬   
+ Help: <b>Buttons</b>
 
 - Bot Supports both url and alert inline buttons.
 
@@ -41,15 +48,23 @@ class script(object):
 <code>[Button Text](buttonurl:https://t.me/username)</code>
 
 <b>Alert buttons:</b>
-<code>[Button Text](buttonalert:This is an alert message)</code>"""
-    AUTOFILTER_TXT = """Help: <b>Auto Filter</b>
+<code>[Button Text](buttonalert:This is an alert message)</code>
+
+▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬"""
+    AUTOFILTER_TXT = """
+▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬    
+Help: <b>Auto Filter</b>
 
 <b>NOTE:</b>
 1. Make me the admin of your channel if it's private.
 2. make sure that your channel does not contains camrips, porn and fake files.
 3. Forward the last message to me with quotes.
- I'll add all the files in that channel to my db."""
-    CONNECTION_TXT = """Help: <b>Connections</b>
+ I'll add all the files in that channel to my db.
+ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬"""
+  CONNECTION_TXT = """  """
+    EXTRAMOD_TXT = """
+ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬
+Help: <b>Connections</b>
 
 - Used to connect bot to PM for managing filters 
 - it helps to avoid spamming in groups.
@@ -61,8 +76,11 @@ class script(object):
 <b>Commands and Usage:</b>
 • /connect  - <code>connect a particular chat to your PM</code>
 • /disconnect  - <code>disconnect from a chat</code>
-• /connections - <code>list all your connections</code>"""
-    EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
+• /connections - <code>list all your connections</code>
+    
+ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬   
+
+Help: <b>Extra Modules</b>
 
 <b>NOTE:</b>
 these are the extra features of Eva Maria
@@ -71,8 +89,11 @@ these are the extra features of Eva Maria
 • /id - <code>get id of a specified user.</code>
 • /info  - <code>get information about a user.</code>
 • /imdb  - <code>get the film information from IMDb source.</code>
-• /search  - <code>get the film information from various sources.</code>"""
-    ADMIN_TXT = """Help: <b>Admin mods</b>
+• /search  - <code>get the film information from various sources.</code>
+▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬"""
+    ADMIN_TXT = """
+ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬
+ Help: <b>Admin mods</b>
 
 <b>NOTE:</b>
 This module only works for my admins
@@ -88,12 +109,16 @@ This module only works for my admins
 • /ban  - <code>to ban a user.</code>
 • /unban  - <code>to unban a user.</code>
 • /channel - <code>to get list of total connected channels</code>
-• /broadcast - <code>to broadcast a message to all users</code>"""
-    STATUS_TXT = """𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
+• /broadcast - <code>to broadcast a message to all users</code>
+ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬"""
+    STATUS_TXT = """
+ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬
+ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
-𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
+𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
+ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬ ▬"""
     LOG_TEXT_G = """#NewGroup
 Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>
